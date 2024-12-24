971c8a5e5d88bd7273fc83649313a6b6d5c32fe0
safi-0412 <omonjanovas@gmail.com>
2024-12-24 21:37:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let username = prompt("enter your name")
+console.log("Name:"  + username);
+
+let name = prompt("enter your username")
+console.log("Username:" + name);
+
+
+let age = prompt("enter your age")
+console.log("Age:" + age);
+
+
+let coutry = prompt("enter your coutry")
+console.log("Country:" + coutry);
